290ef0390cc263f6faded57ab4b1221d373fc83a
William Bigas Mauro <williambmauro@hotmail.com>
2020-05-12 21:40:37 -0300

    Initial commit

---
+++ src/main/java/br/com/senac/usersystemservice/UserSystemServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.usersystemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSystemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSystemServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/senac/usersystemservice/UserSystemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.usersystemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
