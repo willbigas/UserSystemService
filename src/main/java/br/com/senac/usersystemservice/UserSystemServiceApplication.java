a59b19ff372346e13ca3321a24c8f9245ed48a70
Henrique <hike.lemes@gmail.com>
2020-06-20 04:12:12 -0300

    conf para heroku

--- src/main/java/br/com/senac/usersystemservice/UserSystemServiceApplication.java
+++ src/main/java/br/com/senac/usersystemservice/UserSystemServiceApplication.java
@@ -1,11 +1,16 @@
 package br.com.senac.usersystemservice;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -23,6 +28,16 @@ public class UserSystemServiceApplication {
                 registry.addMapping("/**").allowedOrigins("/**");
             }
         };
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
     }
 
 }
